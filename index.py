c8a883df60b7ca05c4228dbc7549bf2df655be17
siwakorn pratumsuwan <maisiwakorn555@gmail.com>
2026-03-02 11:41:59 +0700

    ตกแต่งหน้าHomepage

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
 from flask import Flask,request,render_template,redirect, session
-from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
 
@@ -12,7 +11,6 @@ def init_db():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
     Id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Fullname TEXT NOT NULL,
     Username TEXT NOT NULL,
     Password TEXT NOT NULL,
     Role TEXT DEFAULT 'user'
@@ -24,32 +22,21 @@ def init_db():
 
 
 
-@app.route('/register',methods=["GET", "POST"])
+@app.route('/')
 def home():
+    return render_template("index.html")
 
-    if request.method == "POST":
-        fullname = request.form["fullname"]
-        username = request.form["username"]
-        password = request.form["password"]
+@app.route('/report')
+def report():
+    if 'user' not in session:
+        return redirect('/login')
+    return render_template("report.html")
 
-        conn = sqlite3.connect("mydatabases.db")
-        cursor = conn.cursor()
+@app.route('/login')
+def login():
 
-        cursor.execute("""
-            INSERT INTO users (Fullname, Username, Password)
-            VALUES (?, ?, ?)
-            """, (fullname, username, password))
+    return render_template("login.html")
 
-        conn.commit()
-        conn.close()
-
-    return render_template("register.html")
-
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect("/login")
 
 
 if __name__ == "__main__":
