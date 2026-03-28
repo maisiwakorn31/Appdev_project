46f5f9f38bf58cc903cda434119de33080f9c3b7
siwakorn pratumsuwan <maisiwakorn555@gmail.com>
2026-03-28 16:50:32 +0700

    fix register error handling

--- routes/register.py
+++ routes/register.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from database import get_db
+import sqlite3   # 🔥 ต้องมี
 
 register_bp = Blueprint('register', __name__)
-
 
 @register_bp.route('/register', methods=["GET", "POST"])
 def register():
@@ -14,15 +14,23 @@ def register():
 
         if not phone.isdigit() or len(phone) != 10:
             return render_template("auth.html", error="เบอร์โทรศัพท์ไม่ถูกต้อง")
+
         if password != confirm:
             return render_template("auth.html", error="รหัสผ่านไม่ตรงกัน")
 
         conn = get_db()
-        conn.execute(
-            "INSERT INTO users (fullname, phone, password) VALUES (?,?,?)",
-            (fullname, phone, password)
-        )
-        conn.commit()
+
+        try:
+            conn.execute(
+                "INSERT INTO users (fullname, phone, password) VALUES (?,?,?)",
+                (fullname, phone, password)
+            )
+            conn.commit()
+
+        except sqlite3.IntegrityError:
+            conn.close()
+            return render_template("auth.html", error="เบอร์นี้ถูกใช้แล้ว")
+
         conn.close()
 
         return redirect(url_for("login.login"))
