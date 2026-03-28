527efb04ba456ffea727799f83e8f3cbffc69eb6
siwakorn pratumsuwan <maisiwakorn555@gmail.com>
2026-03-28 16:39:08 +0700

    fix db path for render

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 import sqlite3
+import os
 
 def get_db():
-    conn = sqlite3.connect("users.db")
+    db_path = os.path.join("/tmp", "users.db")  # 🔥 ตรงนี้แหละ
+    conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
 
