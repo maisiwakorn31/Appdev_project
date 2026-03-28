7af1fb029e105dff7365bdc60194207a91be407c
siwakorn pratumsuwan <maisiwakorn555@gmail.com>
2026-03-28 16:47:19 +0700

    fix db path for windows + render

--- database.py
+++ database.py
@@ -2,7 +2,11 @@ import sqlite3
 import os
 
 def get_db():
-    db_path = os.path.join("/tmp", "users.db")  # 🔥 ตรงนี้แหละ
+    if os.name == "nt":
+        db_path = "users.db"
+    else:
+        db_path = "/tmp/users.db"
+
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
